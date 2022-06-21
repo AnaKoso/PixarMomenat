413ffd2a4b6df592cf8112d39d40961df55b179a
keeeeeeeeec <107646542+keeeeeeeeec@users.noreply.github.com>
2022-06-22 00:42:31 +0200

    Delete Project/src/view directory
    proba

--- Project/src/view/TableRowSorter.java
+++
@@ -1,8 +0,0 @@
-package view;
-
-import javax.swing.DefaultRowSorter;
-import javax.swing.table.TableModel;
-
-public class TableRowSorter <M extends TableModel> extends DefaultRowSorter<M,Integer> {
-
-}
